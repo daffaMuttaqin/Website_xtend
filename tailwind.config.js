d8a2cea45b0f615fd35a01d1b2303a5ebc02781c
daffaMuttaqin <nine@upi.edu>
2021-09-13 13:42:33 +0700

    Masukin Tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        marck:['Marck Script'],
+      },
+      gradientColorStops:{
+        'orange' : '#F99500',
+        'purple1' : '#6C63FF',
+        'purple2' : '#675FF4'
+      },
+      colors: {
+        cyan: '#00FFFF',
+        dark_cyan: '#29B6B6'
+      },
+      textColor:{
+        purple: '#6860F5'
+      },
+      borderColor:{
+        purple : '#6860F5'
+      },
+      blur: {
+        xs: '2px',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      display:['group-hover'],
+      padding:['hover'],
+    },
+  },
+  plugins: [],
+}
